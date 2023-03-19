428f78b6685d438865d5ad860e955bb010435125
amosovandrey <rivercity.dp@gmail.com>
2023-03-19 11:47:19 +0200

    js updated

--- js/how-made-video.js
+++ js/how-made-video.js
@@ -1,11 +1,3 @@
-// const poster = document.querySelector(".how-made-poster");
-// poster.addEventListener("click", removePoster);
-
-// function removePoster() {
-//   poster.classList.add("how-made-poster-active");
-//   document.querySelector("#how-made-poster-video").src += "?autoplay=1";
-// }
-
 (() => {
   const refs = {
     pushPlay: document.querySelector('[data-how-made-push-play]'),
